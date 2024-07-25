0a199fa9e9cba994037b1904d4b6b12913c15378
mdabioch <mardab1994@gmail.com>
2024-07-25 22:45:56 +0200

    open image file given as argument

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main(void)
+#include "image/image.h"
+#include "image/loader.h"
+
+int
+main(int argc, const char *argv[])
 {
+    if (argc >= 2) {
+        image_t image;
+        int     result = image_loader(argv[1], &image);
+        printf("result %d \r\n", result);
+        return result;
+    }
+
     return 0;
 }
