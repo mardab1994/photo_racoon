ddff68aacdd95ffd8116b6823a582ec47c37dba7
mdabioch <mardab1994@gmail.com>
2024-07-28 16:45:47 +0200

    gamma filter

--- filters/filters.h
+++ filters/filters.h
@@ -6,3 +6,4 @@ int filters_negative(image_t * const image);
 int filters_thresholding(image_t * const image, const int threshold);
 int filters_half_thresholding_black(image_t *const image, const int threshold);
 int filters_half_thresholding_white(image_t *const image, const int threshold);
+int filters_gamma(image_t *const image, const double gamma);
