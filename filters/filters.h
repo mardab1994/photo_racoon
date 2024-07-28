867b4bbe789cc6dd9a5861b61de3d18b861ecc62
mdabioch <mardab1994@gmail.com>
2024-07-28 16:28:37 +0200

    filters.h declaration of threshold functions

--- filters/filters.h
+++ filters/filters.h
@@ -3,3 +3,6 @@
 #include "image/image.h"
 
 int filters_negative(image_t * const image);
+int filters_thresholding(image_t * const image, const int threshold);
+int filters_half_thresholding_black(image_t *const image, const int threshold);
+int filters_half_thresholding_white(image_t *const image, const int threshold);
